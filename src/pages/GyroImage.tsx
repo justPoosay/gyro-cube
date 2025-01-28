5de233230c6ce80ce5ef31508964f62da92f349f
justPoosai <u50_oscsta_lbn@technischools.com>
2025-01-28 10:57:12 +0100

    Kitty cube

--- src/pages/GyroImage.tsx
+++ src/pages/GyroImage.tsx
@@ -24,35 +24,35 @@ const GyroImage = (): React.ReactElement | null => {
         <div className="gyro-cube" style={cssTransformInverse}>
           <div className="gyro-cube-side gyro-cube-front">
             <img
-              src="https://us-tuna-sounds-images.voicemod.net/6b8bbd21-77dd-4eb9-b014-d93362ee44ae-1661652061871.jpg"
+              src="https://i.pinimg.com/736x/94/a8/d6/94a8d60c5f5dbed03a1f72c0c79680ff.jpg"
               width="250"
               height="250"
             />
           </div>
           <div className="gyro-cube-side gyro-cube-back">
             <img
-              src="https://us-tuna-sounds-images.voicemod.net/6b8bbd21-77dd-4eb9-b014-d93362ee44ae-1661652061871.jpg"
+              src="https://i.pinimg.com/736x/2e/f9/d1/2ef9d12fbb66abfaac9aeb6c3b49f69c.jpg"
               width="250"
               height="250"
             />
           </div>
           <div className="gyro-cube-side gyro-cube-left">
             <img
-              src="https://us-tuna-sounds-images.voicemod.net/6b8bbd21-77dd-4eb9-b014-d93362ee44ae-1661652061871.jpg"
+              src="https://image-cdn-ak.spotifycdn.com/image/ab67706c0000da848e2f48c91de9caf0ed84b982"
               width="250"
               height="250"
             />
           </div>
           <div className="gyro-cube-side gyro-cube-right">
             <img
-              src="https://us-tuna-sounds-images.voicemod.net/6b8bbd21-77dd-4eb9-b014-d93362ee44ae-1661652061871.jpg"
+              src="https://i.pinimg.com/736x/8a/7e/94/8a7e94add4189f3ab19735d11780b072.jpg"
               width="250"
               height="250"
             />
           </div>
           <div className="gyro-cube-side gyro-cube-top">
             <img
-              src="https://us-tuna-sounds-images.voicemod.net/6b8bbd21-77dd-4eb9-b014-d93362ee44ae-1661652061871.jpg"
+              src="https://i.pinimg.com/736x/c3/9a/c5/c39ac5ebf279969584e8e12b7622c556.jpg"
               width="250"
               height="250"
             />
