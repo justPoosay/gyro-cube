5e7a1f7d82cc15da210e4c881c6b1a98f46cd7fb
justPoosai <u50_oscsta_lbn@technischools.com>
2025-01-28 13:48:51 +0100

    quick update

--- src/pages/GyroHue.tsx
+++ src/pages/GyroHue.tsx
@@ -16,13 +16,13 @@ const GyroHue = (): React.ReactElement | null => {
   // Zmieniamy położenie gradientu w odpowiedzi na orientację urządzenia
   useEffect(() => {
     if (orientation) {
-      const { beta, gamma } = orientation;
+      const { alpha, beta, gamma } = orientation;
 
       // Przesuwamy gradient na podstawie orientacji
-      const translateX = gamma ? gamma * 1.5 : 0; // Skala dla gamma
-      const translateY = beta ? beta * 1.5 : 0; // Skala dla beta
+      const translateX = gamma ? gamma * 10 : 0; // Skala dla gamma
+      const translateY = beta ? beta * 10 : 0; // Skala dla beta
 
-      // Ustawiamy transformację dla gradientu
+      // Ustawiamy transformację tylko dla gradientu
       setTransform(`translate(${translateX}px, ${translateY}px)`);
     }
   }, [orientation]);
@@ -38,53 +38,52 @@ const GyroHue = (): React.ReactElement | null => {
         </button>
       )}
 
-      <div className="mt-5 relative w-64 h-64">
+      <div className="mt-5 relative w-full h-full">
+        {/* Kontener dla gradientu, który pokrywa całą stronę */}
         <div
-          className="relative w-full h-full rounded-lg overflow-hidden"
+          className="absolute w-full h-full"
           style={{
             zIndex: 10,
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "103px",
-            height: "121px",
-            background: "linear-gradient(45deg, rgba(255,0,255,0.6), rgba(0,255,255,0.6), rgba(255,255,0,0.6))",
-            backgroundSize: "100% 100%",
-            // Przemieszczamy gradient w odpowiedzi na orientację
-            transform: transform,
-            clipPath: "url(#svgClip)", // Używamy clip-path, aby przyciąć do kształtu SVG
-            overflow: "hidden",
+            background:
+              "linear-gradient(45deg, rgba(255,0,255,0.6), rgba(0,255,255,0.6), rgba(255,255,0,0.6))",
+            backgroundSize: "200% 200%",
+            transform: transform, // Przemieszczamy gradient
+            clipPath: "url(#svgClip)", // Przycinamy gradient do kształtu SVG
+            overflow: "hidden", // Zapobiegamy wystawaniu gradientu poza granice
           }}
-        >
-          <img
-            src="/godlo.svg"
-            alt="Holographic Sticker"
-            style={{
-              filter: "opacity(0.5) grayscale(1)",
-              width: "100%",
-              height: "100%",
-              objectFit: "cover",
-            //   transform: transform,
-            }}
-            className="w-full h-full object-cover rounded-lg shadow-lg"
-          />
+        />
 
-          <svg width="0" height="0">
-            <defs>
-              <clipPath id="svgClip">
-                <path
-                  style={{
-                    stroke: "none",
-                    fillRule: "evenodd",
-                    fill: "rgb(86.274511%,7.843138%,23.529412%)",
-                    fillOpacity: 1,
-                  }}
-                  d="M 0 0 L 1.351562 96.421875 C 1.351562 96.421875 1.472656 105.101562 10.292969 110.867188 C 12.753906 112.476562 16.675781 113.75 22.695312 113.667969 C 24.527344 113.640625 35.027344 113.589844 40.316406 113.734375 C 44.175781 113.839844 50.601562 116.511719 51.054688 120.992188 L 51.960938 120.992188 C 52.347656 115.804688 58.777344 113.847656 62.699219 113.734375 C 67.960938 113.585938 78.484375 113.644531 80.316406 113.667969 C 86.335938 113.75 90.257812 112.476562 92.71875 110.867188 C 101.539062 105.101562 101.660156 96.421875 101.660156 96.421875 L 103.011719 0 Z M 0 0 "
-                />
-              </clipPath>
-            </defs>
-          </svg>
-        </div>
+        {/* Obrazek pozostaje w swoich pierwotnych wymiarach i nie przesuwa się */}
+        <img
+          src="/godlo.svg"
+          alt="Holographic Sticker"
+          style={{
+            position: "absolute",
+            top: "50%", // Ustawiamy go na środku
+            left: "50%",
+            transform: "translate(-50%, -50%)", // Wyśrodkowanie
+            width: "103px", // Oryginalna szerokość obrazka
+            height: "121px", // Oryginalna wysokość obrazka
+            objectFit: "cover", // Obrazek wypełnia kontener
+            filter: "opacity(0.5) grayscale(1)", // Efekty na obrazku
+          }}
+        />
+
+        <svg width="0" height="0">
+          <defs>
+            <clipPath id="svgClip">
+              <path
+                style={{
+                  stroke: "none",
+                  fillRule: "evenodd",
+                  fill: "rgb(86.274511%,7.843138%,23.529412%)",
+                  fillOpacity: 1,
+                }}
+                d="M 0 0 L 1.351562 96.421875 C 1.351562 96.421875 1.472656 105.101562 10.292969 110.867188 C 12.753906 112.476562 16.675781 113.75 22.695312 113.667969 C 24.527344 113.640625 35.027344 113.589844 40.316406 113.734375 C 44.175781 113.839844 50.601562 116.511719 51.054688 120.992188 L 51.960938 120.992188 C 52.347656 115.804688 58.777344 113.847656 62.699219 113.734375 C 67.960938 113.585938 78.484375 113.644531 80.316406 113.667969 C 86.335938 113.75 90.257812 112.476562 92.71875 110.867188 C 101.539062 105.101562 101.660156 96.421875 101.660156 96.421875 L 103.011719 0 Z M 0 0 "
+              />
+            </clipPath>
+          </defs>
+        </svg>
       </div>
     </div>
   );
