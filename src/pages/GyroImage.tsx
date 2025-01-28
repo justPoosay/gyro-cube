b2910f602a26dba51a242ab6094139da69bc2581
justPoosai <u50_oscsta_lbn@technischools.com>
2025-01-28 10:49:48 +0100

    Sans cube???

--- src/pages/GyroImage.tsx
+++ src/pages/GyroImage.tsx
@@ -1,30 +1,47 @@
-import { useState } from "react";
-// import sansDance from "../assets/sans-dance.png";
+import React from "react";
 import { useDeviceOrientation } from "../useDeviceOrientation";
+import OrientationSwitcher from "../OrientationSwitcher";
+import "./GyroCube.css";
 
 const GyroImage = (): React.ReactElement | null => {
-  const { orientation, requestAccess } = useDeviceOrientation();
-  const [accessGranted, setAccessGranted] = useState(false);
+  const { requestAccess, revokeAccess, cssTransformInverse } =
+    useDeviceOrientation();
 
-  const handleRequestAccess = async () => {
-    const granted = await requestAccess();
-    setAccessGranted(granted);
+  const onToggle = (toggleState: boolean): void => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    toggleState ? requestAccess() : revokeAccess();
   };
 
-  // Obliczanie wartości hue na podstawie alpha (obrót wokół osi Z)
-  const hue = orientation?.alpha ? orientation.alpha % 360 : 0;
-
   return (
-    <div>
-      {!accessGranted && (
-        <button onClick={handleRequestAccess}>Zezwól na czujniki</button>
-      )}
-      <img
-        // src={sansDance}
-        src="https://us-tuna-sounds-images.voicemod.net/6b8bbd21-77dd-4eb9-b014-d93362ee44ae-1661652061871.jpg"
-        alt="Gyro-controlled"
-        style={{ filter: `hue-rotate(${hue}deg)` }}
+    <div className="post--2021--water-line--gyro-cube">
+      <OrientationSwitcher
+        onToggle={onToggle}
+        labelOff="Turn the Gyro-Cube ON"
+        labelOn="Turn the Gyro-Cube OFF"
       />
+
+      <div className="gyro-cube-container">
+        <div className="gyro-cube" style={cssTransformInverse}>
+          <div className="gyro-cube-side gyro-cube-front">
+            <img src="https://us-tuna-sounds-images.voicemod.net/6b8bbd21-77dd-4eb9-b014-d93362ee44ae-1661652061871.jpg" />
+          </div>
+          <div className="gyro-cube-side gyro-cube-back">
+            <img src="https://us-tuna-sounds-images.voicemod.net/6b8bbd21-77dd-4eb9-b014-d93362ee44ae-1661652061871.jpg" />
+          </div>
+          <div className="gyro-cube-side gyro-cube-left">
+            <img src="https://us-tuna-sounds-images.voicemod.net/6b8bbd21-77dd-4eb9-b014-d93362ee44ae-1661652061871.jpg" />
+          </div>
+          <div className="gyro-cube-side gyro-cube-right">
+            <img src="https://us-tuna-sounds-images.voicemod.net/6b8bbd21-77dd-4eb9-b014-d93362ee44ae-1661652061871.jpg" />
+          </div>
+          <div className="gyro-cube-side gyro-cube-top">
+            <img src="https://us-tuna-sounds-images.voicemod.net/6b8bbd21-77dd-4eb9-b014-d93362ee44ae-1661652061871.jpg" />
+          </div>
+          <div className="gyro-cube-side gyro-cube-bottom">
+            <img src="https://us-tuna-sounds-images.voicemod.net/6b8bbd21-77dd-4eb9-b014-d93362ee44ae-1661652061871.jpg" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
